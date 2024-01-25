43fb17d5ca26cb71e8b6cd5f9db7db1522258042
ValtheCoder <keshia.kdf@gmail.com>
2024-01-25 14:36:05 +0800

    full income and tax to pay

--- Calculateincometax.py
+++ Calculateincometax.py
@@ -1,3 +1,6 @@
 # Income is $45000
+full_income = 45000
 # Tax to pay is %6000
-# 10000*0% + 10000*10% + 25000*20% = 6000
+# Lets say we dont know how much tax we need to pay
+tax_2pay = 0
+# 10000*0% + 10000*10% + 25000*20% = 6000, this is the guide
